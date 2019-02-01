5eb1f00686f260c8961c5f015b42eb98271c8f20
Jason loh <ksloh@hotmail.my>
2019-02-01 10:37:32 +0800

    Add files via upload
    - added script to run Gong

---
+++ servo.js
@@ -0,0 +1,10 @@
+const Raspi = require('raspi-io');
+const five = require("johnny-five");
+const board = new five.Board({
+    io: new Raspi()
+  });
+
+board.on("ready", () => {
+  const servo = new five.Servo('GPIO18');
+  servo.center();
+})
